fe765ad5c3aa4d05ccf77ad8f047d2e0825151b7
reyhancode12 <104006836+reyhancode12@users.noreply.github.com>
2022-07-20 17:31:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name="reyhan";
+var last_name="ravi";
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var r= 12;
+var n= 5;
+
+var add=r+n;
+console.log(add);
+
+var q= 70;
+var e= 7;
+
+var multiply=q*e;
+console.log(multiply);
